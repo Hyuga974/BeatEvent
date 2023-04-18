891968b0d8cfffd12acd2d18f718089cc95a9f56
Hyuga <tamashiyami22@gmail.com>
2023-04-18 17:38:16 +0200

    Begin first window

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from tkinter import *
+window=Tk()
+lbl=Label(window, text="Hello Wrold", fg='red', font=("Helvetica", 16))
+lbl.place(x=60, y=50)
+window.title('Hello Python')
+window.geometry("300x200+10+10")
+window.mainloop()
+
